2615b2507379073e345385b0c28c35b8351a1f40
Alec LaLonde <alec.lalonde@bluereview.net>
2014-08-26 10:44:20 -0600

    Added hooks for keyup and click listeners

--- src/tinymce.js
+++ src/tinymce.js
@@ -1,14 +1,37 @@
 /**
  * Binds a TinyMCE widget to <textarea> elements.
+ *
+ * Forked to git@github.com:alalonde/ui-tinymce.git
+ *
+ * With this version you can add keyup and click listeners to the TinyMCE textarea.
+ * It also overrides the tab and shift-tab keys to indent/outdent rather than switch focus.
  */
 angular.module('ui.tinymce', [])
   .value('uiTinymceConfig', {})
   .directive('uiTinymce', ['uiTinymceConfig', function (uiTinymceConfig) {
+    'use strict';
+
     uiTinymceConfig = uiTinymceConfig || {};
-    var generatedIds = 0;
+
+    // keep track of all the listeners, as there may be one per instance of this directive
+    var generatedIds = 0, keyUpListeners = [], clickListeners = [];
+
+    function parseId(id) {
+      return parseInt(id.substr(id.length - 1), 10);
+    }
+
     return {
       priority: 10,
       require: 'ngModel',
+      controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
+      // define an API for child directives to view and modify sorting parameters
+        this.setKeyUpListener = function(listener) {
+          keyUpListeners[parseId($attrs.id)] = listener; 
+        };
+        this.setClickListener = function(listener) {
+          clickListeners[parseId($attrs.id)] = listener;
+        };
+      }],
       link: function (scope, elm, attrs, ngModel) {
         var expression, options, tinyInstance,
           updateView = function () {
@@ -50,8 +73,30 @@ angular.module('ui.tinymce', [])
             });
             // Update model on keypress
             ed.on('KeyUp', function (e) {
+              var listener = keyUpListeners[parseId(attrs.id)];
+              if(listener) {
+                listener(ed, e);
+              }
               ed.save();
               updateView();
+            });
+            ed.on('KeyDown', function(event) {
+              if (event.keyCode === 9) { // tab pressed
+                if (event.shiftKey) {
+                  ed.execCommand('Outdent');
+                } else {
+                  ed.execCommand('Indent');
+                }
+
+                event.preventDefault();
+                return false;
+              }
+            });
+            ed.on('Click', function(e) {
+              var listener = clickListeners[parseId(attrs.id)];
+              if(listener) {
+                listener(ed, e);
+              }
             });
             // Update model on change, i.e. copy/pasted text, plugins altering content
             ed.on('SetContent', function (e) {
@@ -60,8 +105,8 @@ angular.module('ui.tinymce', [])
                 updateView();
               }
             });
-            ed.on('blur', function(e) {
-                elm.blur();
+            ed.on('Blur', function(e) {
+              elm.blur();
             });
             // Update model when an object has been resized (table, image)
             ed.on('ObjectResized', function (e) {
@@ -91,7 +136,9 @@ angular.module('ui.tinymce', [])
         };
 
         scope.$on('$destroy', function() {
-          if (!tinyInstance) { tinyInstance = tinymce.get(attrs.id); }
+          if (!tinyInstance) { 
+            tinyInstance = tinymce.get(attrs.id); 
+          }
           if (tinyInstance) {
             tinyInstance.remove();
             tinyInstance = null;
